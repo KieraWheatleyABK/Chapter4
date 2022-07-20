120d11424aaec04cee8f8f5816e84574ee759cf4
Kiera Wheatley <kiera.wheatley@activision.com>
2022-07-20 09:04:43 -0400

    init

---
+++ Ch4Challenge2/Ch4Challenge2/Ch4Challenge2.cpp
@@ -0,0 +1,11 @@
+// Ch4Challenge2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    return 0;
+}

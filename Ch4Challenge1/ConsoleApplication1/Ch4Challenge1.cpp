6d7480bfa447d559809a7e027968e0d8277f2d7c
Kiera Wheatley <kiera.wheatley@activision.com>
2022-07-20 08:54:51 -0400

    final solution

--- Ch4Challenge1/ConsoleApplication1/Ch4Challenge1.cpp
+++ Ch4Challenge1/ConsoleApplication1/Ch4Challenge1.cpp
@@ -5,7 +5,52 @@
 
 using namespace std;
 
+void DisplayArray(int arr[], int size);
+void DisplayLargestValue(int arr[], int size);
+int GetLargestValue(int arr[], int size);
+
+void DisplayArray(int arr[], int size)
+{
+    cout << "The array contents are: {";
+
+    for (int i = 0; i < size; i++)
+    {
+        if (i != size - 1)
+        {
+            cout << arr[i] << ", ";
+        }
+        else
+        {
+            cout << arr[i] << "}" << endl;
+        }      
+    }
+}
+
+void DisplayLargestValue(int arr[], int size)
+{
+    cout << "The largest value in the array is " << GetLargestValue(arr, size) << "." << endl;
+}
+
+int GetLargestValue(int arr[], int size)
+{
+    int max = arr[0];
+
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+
+    return max;
+}
+
 int main()
 {
-    return 0;
+    int a[] = { 100, 1000, -4, 10, 300 };
+    size_t n = sizeof(a) / sizeof(a[0]);
+
+    DisplayArray(a, n);
+    DisplayLargestValue(a, n);
 }
